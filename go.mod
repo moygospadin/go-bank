e73d42872b62850244ee3d4c304145dccf7b706d
aaa <123@mm.mm>
2024-05-22 17:11:39 +0200

    ddd

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/moygospadin/go-bank
 
 go 1.18
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/lib/pq v1.10.9
+)
---
+++ storage.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+type Storage interface {
+	CreateAccount(account *Account) error
+	DeleteAccount(int) error
+	UpdateAccount(account *Account) error
+	GetAccountByID(int) (*Account, error)
+	GetAccounts() ([]*Account, error)
+}
+
+type PostgresStore struct {
+	db *sql.DB
+}
+
+func NewPostgresStore() (*PostgresStore, error) {
+	connStr := "user=postgres dbname=postgres password=gobank sslmode=disable host=localhost port=5432"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return &PostgresStore{db: db}, nil
+}
+
+func (s *PostgresStore) Init() error {
+	return s.createAccountTable()
+}
+
+func (s *PostgresStore) createAccountTable() error {
+	query := `CREATE TABLE IF NOT EXISTS account (
+		id serial primary key,
+		first_name varchar(50),
+		last_name varchar(50),
+		number serial,
+		balance serial,
+		created_at timestamp
+		)`
+	_, err := s.db.Exec(query)
+	return err
+}
+
+func (s *PostgresStore) CreateAccount(account *Account) error {
+	query := `
+	insert into account 
+    (first_name, last_name, number, balance,created_at)
+	values
+	($1, $2, $3, $4, $5)
+	`
+	resp, err := s.db.Query(query, account.FirstName, account.LastName, account.Number, account.Balance, account.CreatedAt)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%+v\n", resp)
+	return nil
+}
+
+func (s *PostgresStore) UpdateAccount(account *Account) error {
+	return nil
+}
+
+func (s *PostgresStore) DeleteAccount(id int) error {
+	return nil
+}
+
+func (s *PostgresStore) GetAccountByID(id int) (*Account, error) {
+	return nil, nil
+}
+
+func (s *PostgresStore) GetAccounts() ([]*Account, error) {
+	rows, err := s.db.Query("select * from account")
+	if err != nil {
+		return nil, err
+	}
+	accounts := []*Account{}
+
+	for rows.Next() {
+		account := new(Account)
+		err := rows.Scan(
+			&account.ID,
+			&account.FirstName,
+			&account.LastName,
+			&account.Number,
+			&account.Balance,
+			&account.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		accounts = append(accounts, account)
+	}
+	return accounts, nil
+}
